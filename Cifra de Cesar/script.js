f9537b57faabb5efd459b34a95a7b547613d9dc2
Allan Kildare <allankildare27@gmail.com>
2020-10-13 17:29:43 -0300

    cifra de cesar adicionada

---
+++ Cifra de Cesar/script.js
@@ -0,0 +1,50 @@
+// Primeira parte concluida
+function cifraComCifraDeCesar(deslocamento, mensagem)
+{
+ var mensagemEncriptada = "";
+
+ for (let letra of mensagem){
+
+    if (deslocamento > 26){
+        deslocamento = 26;
+    }
+    var letraNumber = letra.charCodeAt() + deslocamento;
+
+    if (letraNumber >= 90){
+        letraNumber = letraNumber - 26;
+    }
+
+    mensagemEncriptada +=  String.fromCharCode(letraNumber)
+ }
+
+ return mensagemEncriptada;
+}
+
+console.log(cifraComCifraDeCesar(1, "FELIPEZ"))
+
+function decifraCifraDeCesar( deslocamento, mensagemCifrada )
+{
+ var mensagemDecifrada = "";
+
+ for (let letra of mensagemCifrada){
+
+    if (deslocamento > 26){
+        deslocamento = 26;
+    }
+    var letraNumber = letra.charCodeAt() - deslocamento;
+
+    if (letraNumber < 65){
+        letraNumber = letraNumber + 26;
+    }
+
+    mensagemDecifrada +=  String.fromCharCode(letraNumber);
+ }
+
+ 
+
+ return mensagemDecifrada;
+}
+
+console.log(decifraCifraDeCesar(1, "GFMJQFA"))
+
+//String.fromCharCode()//
