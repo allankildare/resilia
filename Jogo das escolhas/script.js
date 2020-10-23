57fb44953f809819c8d43673d4d13b8bce79ce10
Allan Kildare <allankildare27@gmail.com>
2020-10-22 21:29:19 -0300

    jogo melhorado

--- Jogo das escolhas/script.js
+++ Jogo das escolhas/script.js
@@ -1,117 +1,113 @@
+// utilidades
 
-// Primeira Fase
+function fimDeJogo () { // botao para fim de jogo
+    // remove botao de escolhas
+    document.getElementById("choice-1").remove()
+    document.getElementById("choice-2").remove()
+
+    // cria botao
+    var button = document.createElement("button")
+    var textButton = document.createTextNode("Fim de jogo")
+    button.appendChild(textButton)
+
+    // arrow function para definir fim de jogo
+    button.onclick = () => window.location.reload()
+    
+    var container = document.getElementById("container")
+    container.appendChild(button)
+}
+
+function textoParagrafo (texto) { // texto do paragrafo
+    var paragrafo = document.getElementById("game-text")
+    paragrafo.textContent = texto
+}
+
+function faseFamoso (famoso) { // troca nome do famoso
+    var famosoTech = document.getElementById("tech-famous")
+    famosoTech.textContent = famoso
+}
+
+function substituiBotao (numeroOpcao, texto, funcao) { // substitui texto do botao de acordo com o id, chama funcao
+    var botao = document.getElementById(`choice-${numeroOpcao}`)
+    botao.textContent = texto
+    botao.onclick = funcao
+}
+
+function insereImagem (nomeArquivo, textoAlt) {
+    var container = document.getElementById("container")
+    container.innerHTML += `<img src="./images/${nomeArquivo}" alt="${textoAlt}">`
+
+    container.innerHTML += '<br>'
+}
+
+// Fase 1
 function iniciar () {
+    var container = document.getElementById("container")
     var nome = document.getElementById("name").value
-    var body = document.querySelector("body")
+    var textoFase = `Ok, vamos iniciar uma história.<br>
+    Eu trabalho no Fakebook, mas você, ${nome}, está planejando entrar num emprego de desenvolvimento web.<br>
+    Você está num mundo onde o coronavírus é realidade. Você vai sair para entregar currículo?`
+    var paragrafo = `<p id="game-text">` + textoFase + `</p>`
+    var famoso = "Marcos Zuckerberg"
+    document.getElementById("name-enter").remove()
 
-    body.innerHTML += `<p>Ok, vamos iniciar uma história.<br>
-    Você, ${nome}, está planejando entrar num emprego de desenvolvimento web.<br>
-    Mas você está num mundo onde o coronavírus é realidade. Você vai sair para entregar currículo?<br>`
-    body.innerHTML += `<button onclick="primeiraSim()">Sim</button>
-    <button onclick="primeiraNao()">Não</button>`
+    container.innerHTML += `<p class="text-start" id="tech-famous">${famoso}</p>`
+    container.innerHTML += paragrafo
+    container.innerHTML += `<button onclick="primeiraSim()" id="choice-1">SIM</button>
+    <button onclick="primeiraNao()" id="choice-2">Não</button>`
+}
+
+
+// Fase 2
+function primeiraSim () {
+    textoParagrafo("Poxa,agora você está com coronavírus. Passar bem :)")
     
+    document.getElementById("choice-1").remove()
+    document.getElementById("choice-2").remove()
     
+    fimDeJogo()
 }
 
 function primeiraNao () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Ufa, pelo menos sua saúde foi salva.<br>
-    Mas e aí, estudou bem o HTML?`
-    body.innerHTML += `<br>`
-    body.innerHTML += `<button onclick="segundaSim()">Sim</button>
-    <button onclick="segundaNao()">Não</button>`
+    faseFamoso("Elão Musk")
+    textoParagrafo("Ufa, pelo menos sua saúde foi salva.\n Mas e aí, como você declara variável no JavaScript?")
+    
+    substituiBotao(1, "var", segundaVar)
+    substituiBotao(2, "variavel", segundaVariavel)
 }
 
-function primeiraSim () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Putz, agora você pegou o corona e está em estado grave. Passar bem =(`
-    body.innerHTML += `<br><button onclick="location.reload()">Voltar</button>`
+// Fase 3
+function segundaVar () {
+    faseFamoso("Bill Portões")
+    textoParagrafo("Boa! Começamos bem.\nJá conhece o Microsof Edge?\nQual a linguagem de programação nativa dos navegadores?")
+    
+    substituiBotao(1, "HTML", terceiraHtml)
+    substituiBotao(2, "JavaScript", terceiraJS)
 }
 
-// Segunda Fase
-function segundaSim () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Ótimo, estudou bem CSS?`
-    body.innerHTML += `<br>`
-    body.innerHTML += `<button onclick="terceiraSim()">Sim</button>
-    <button onclick="terceiraNao()">Não</button>`
+function segundaVariavel () {
+    textoParagrafo("A palavra reservada correta é var. O JavaScript não foi programa utilizando o português.")
+
+    insereImagem("errou-faustao.gif", "Errou (meme do Faustão)")
+
+    fimDeJogo()
 }
 
-function segundaNao () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Estude mais, vai dar certo!`
-    body.innerHTML += `<br><button onclick="location.reload()">Voltar</button>`
+// Fase 4
+function terceiraJS () {
+    faseFamoso("Ada AmaRenda")
+    textoParagrafo("Boa! Você sabe o que é linguagem de programação. Eu meio que criei ela sabe, mas eu esqueci uma coisa,\nQual dessas estruturas é um laço de repetição?")
+    
+    substituiBotao(1, "function", quartaFunction)
+    substituiBotao(2, "for", quartaFor)
 }
 
-// Terceira Fase
-function terceiraSim () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Melhor ainda, E o JavaScript hein?`
-    body.innerHTML += `<br>`
-    body.innerHTML += `<button onclick="quartaSim()">Sim</button>
-    <button onclick="quartaNao()">Não</button>`
+function terceiraHtml () {
+    textoParagrafo("O HTML é apenas uma linguagem de marcação, o JavaScript que é de programação")
+
+    insereImagem("eh.gif", "Rosto desconfiado, você errou")
+
+    fimDeJogo()
 }
 
-function terceiraNao () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Linguagem de programação é super importante! Seja pro back ou front-end.<br>
-    Para fullstack é essencial, volte e melhore um pouquinho mais.`
-    body.innerHTML += `<br><button onclick="location.reload()">Voltar</button>`
-}
-
-// Quarta Fase
-function quartaSim () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Caramba, Você tá indo bem! Acho que vale a pena você explorar o React, 
-    React Native, jQuery ou outras bibliotecas e frameworks que utilizam o JavaScript como base. 
-    Mas calma, e o Node.js? Estudou?`
-    body.innerHTML += `<br>`
-    body.innerHTML += `<button onclick="quintaSim()">Sim</button>
-    <button onclick="quintaNao()">Não</button>`
-}
-
-function quartaNao () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Linguagem de programação é super importante! Seja pro back ou front-end.<br>
-    Para fullstack é essencial, volte e melhore um pouquinho mais.`
-    body.innerHTML += `<br><button onclick="location.reload()">Voltar</button>`
-}
-
-// Quinta Fase
-function quintaSim () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Boa! E as soft skills?`
-    body.innerHTML += `<br>`
-    body.innerHTML += `<button onclick="sextaSim()">Sim</button>
-    <button onclick="sextaNao()">Não</button>`
-}
-
-function quintaNao () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Muito importante entender o funcionamento do Node.js, se eu fosse você, estudaria.`
-    body.innerHTML += `<br><button onclick="location.reload()">Voltar</button>`
-}
-
-// Sexta Fase
-function sextaSim () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Parabéns! Já dá pra ser um junior!`
-}
-
-function sextaNao () {
-    var body = document.querySelector("body")
-    body.innerHTML += `<br>`
-    body.innerHTML += `Exerça elas, são fundamentais pro mercado de trabalho.`
-    body.innerHTML += `<br><button onclick="location.reload()">Voltar</button>`
-}
+// Finalizado até a fase 3 por enquanto
